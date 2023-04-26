81fa2626bd0d02e246414c35c17635eaa01aef93
Dave <david.jaehyeok.choi@gmail.com>
2023-04-26 19:22:19 +0900

    adding new page

--- midterm/lib/main.dart
+++ midterm/lib/main.dart
@@ -30,104 +30,128 @@ class BudgetScreenState extends State<BudgetScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('My Budget'),
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-              child: Text(
-                'Income',
-                style: TextStyle(fontSize: 24.0),
-              ),
+      appBar: AppBar(
+        title: const Text('My Budget'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            child: Text(
+              'Income',
+              style: TextStyle(fontSize: 24.0),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: TextField(
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                  hintText: 'Enter your monthly income',
-                ),
-                onChanged: (value) {
-                  setState(() {
-                    _income = double.tryParse(value) ?? 0.0;
-                  });
-                },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: TextField(
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                hintText: 'Enter your monthly income',
               ),
+              onChanged: (value) {
+                setState(() {
+                  _income = double.tryParse(value) ?? 0.0;
+                });
+              },
             ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-              child: Text(
-                'Expenses',
-                style: TextStyle(fontSize: 24.0),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            child: Text(
+              'Expenses',
+              style: TextStyle(fontSize: 24.0),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: TextField(
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                hintText: 'Enter your monthly expenses',
               ),
+              onChanged: (value) {
+                setState(() {
+                  _expenses = double.tryParse(value) ?? 0.0;
+                });
+              },
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: TextField(
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                  hintText: 'Enter your monthly expenses',
-                ),
-                onChanged: (value) {
-                  setState(() {
-                    _expenses = double.tryParse(value) ?? 0.0;
-                  });
-                },
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            child: Text(
+              'Savings Goal',
+              style: TextStyle(fontSize: 24.0),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: TextField(
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                hintText: 'Enter your savings goal',
               ),
+              onChanged: (value) {
+                setState(() {
+                  _savingsGoal = double.tryParse(value) ?? 0.0;
+                });
+              },
             ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-              child: Text(
-                'Savings Goal',
-                style: TextStyle(fontSize: 24.0),
-              ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            child: Text(
+              'Total Remaining',
+              style: TextStyle(fontSize: 24.0),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: TextField(
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                  hintText: 'Enter your savings goal',
-                ),
-                onChanged: (value) {
-                  setState(() {
-                    _savingsGoal = double.tryParse(value) ?? 0.0;
-                  });
-                },
-              ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Text(
+              '\$${(_income - _expenses).toStringAsFixed(2)}',
+              style:
+                  const TextStyle(fontSize: 36.0, fontWeight: FontWeight.bold),
             ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-              child: Text(
-                'Total Remaining',
-                style: TextStyle(fontSize: 24.0),
-              ),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+            child: Text(
+              'Savings Progress',
+              style: TextStyle(fontSize: 24.0),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Text(
-                '\$${(_income - _expenses).toStringAsFixed(2)}',
-                style: const TextStyle(
-                    fontSize: 36.0, fontWeight: FontWeight.bold),
-              ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: LinearProgressIndicator(
+              value: _income == 0.0 ? 0.0 : _expenses / _income,
             ),
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-              child: Text(
-                'Savings Progress',
-                style: TextStyle(fontSize: 24.0),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: LinearProgressIndicator(
-                value: _income == 0.0 ? 0.0 : _expenses / _income,
-              ),
-            ),
-          ],
-        ));
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const NewPage()),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class NewPage extends StatelessWidget {
+  const NewPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('내역 조회'),
+      ),
+      body: const Center(child: Text('This is a new page.')),
+    );
   }
 }
