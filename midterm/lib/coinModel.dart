1446d95aa1d66aa3329f3362ec5b99ed88772046
chul6 <hyungchul0604@gmail.com>
2023-06-20 20:13:11 +0900

    imageUrl -> image (fix json call)

--- midterm/lib/coinModel.dart
+++ midterm/lib/coinModel.dart
@@ -4,7 +4,7 @@ class Coin {
   Coin({
     required this.name,
     required this.symbol,
-    required this.imageUrl,
+    required this.image,
     required this.price,
     required this.change,
     required this.changePercentage,
@@ -12,7 +12,7 @@ class Coin {
 
   String name;
   String symbol;
-  String imageUrl;
+  String image;
   num price;
   num change;
   num changePercentage;
@@ -21,7 +21,7 @@ class Coin {
     return Coin(
       name: json['name'],
       symbol: json['symbol'],
-      imageUrl: json['image'],
+      image: json['image'],
       price: json['current_price'],
       change: json['price_change_24h'],
       changePercentage: json['price_change_percentage_24h'],
