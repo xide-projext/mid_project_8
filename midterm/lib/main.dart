29cc1c8dc9c092d4de26ce504b514ecee729668f
chul6 <hyungchul0604@gmail.com>
2023-06-20 20:11:09 +0900

    StockPageContent - API, json call

--- midterm/lib/main.dart
+++ midterm/lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+
+import 'package:midterm/coinModel.dart';
+import 'package:midterm/coinCard.dart';
+
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:table_calendar/table_calendar.dart';
-
-
 
 void main() => runApp(const BudgetApp());
 
@@ -63,7 +67,7 @@ class MainPageState extends State<MainPage> {
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.attach_money),
-            label: '주식',
+            label: '투자',
           ),
         ],
       ),
@@ -638,173 +642,66 @@ class ExtraBudgetScreenState extends State<ExtraBudgetScreen> {
   }
 }
 
-class StockPageContent extends StatelessWidget {
+class StockPageContent extends StatefulWidget {
   const StockPageContent({Key? key}) : super(key: key);
 
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-        scrollDirection: Axis.vertical,
-        children: [
-          CoinCard(
-            name: 'Bitcoin',
-            symbol: 'btc',
-            imageUrl: 
-                'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579', 
-            price: 36273, 
-            change: 933.13, 
-            changePercentage: 2.6,
-          ),
-        ],
-      ),
-    );
-  }
+  _StockPageContentState createState() => _StockPageContentState();
 }
 
-class CoinCard extends StatelessWidget {
-  CoinCard({
-    required this.name,
-    required this.symbol,
-    required this.imageUrl,
-    required this.price,
-    required this.change,
-    required this.changePercentage,
-  });
+class _StockPageContentState extends State<StockPageContent> {
+  late List<Coin> coinList;
 
+  Future<List<Coin>> fetchCoin() async {
+    coinList = [];
+    final response = await http.get(Uri.parse(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc'));
 
-  String name;
-  String symbol;
-  String imageUrl;
-  double price;
-  double change;
-  double changePercentage;
+    if (response.statusCode == 200) {
+      List<dynamic> values = [];
+      values = json.decode(response.body);
+      if (values.length > 0) {
+        for (int i = 0; i < values.length; i++) {
+          if (values[i] != null) {
+            Map<String, dynamic> map = values[i];
+            coinList.add(Coin.fromJson(map));
+          }
+        }
+        setState(() {
+          coinList;
+        });
+      }
+      return coinList;
+    } else {
+      throw Exception('Failed to load coins');
+    }
+  }
+
+  @override
+  void initState() {
+    fetchCoin();
+    Timer.periodic(const Duration(seconds: 10), (timer) => fetchCoin());
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 15, left: 10, right: 10),
-      child: Container(
-        height: 100,
-        decoration: BoxDecoration(
-          color: Colors.grey[300],
-          borderRadius: BorderRadius.circular(20),
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.grey,
-              offset: Offset(4, 4),
-              blurRadius: 10,
-              spreadRadius: 1,
-            ),
-            BoxShadow(
-              color: Colors.white,
-              offset: Offset(-4, -4),
-              blurRadius: 10,
-              spreadRadius: 1,
-            ),
-          ],
-        ),
-        child: Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.grey[300],
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: const [
-                    BoxShadow(
-                      color: Colors.grey,
-                      offset: Offset(4, 4),
-                      blurRadius: 10,
-                      spreadRadius: 1,
-                    ),
-                    BoxShadow(
-                      color: Colors.white,
-                      offset: Offset(-4, -4),
-                      blurRadius: 10,
-                      spreadRadius: 1,
-                    ),
-                  ],
-                ),
-                height: 60,
-                width: 60,
-                child: Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Image.network(imageUrl),
-                ),
-              ),
-            ),
-            Expanded(
-              child: Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    FittedBox(
-                      fit: BoxFit.scaleDown,
-                      child: Text(
-                        name,
-                        style: TextStyle(
-                          color: Colors.grey[900],
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                    Text(
-                      symbol,
-                      style: TextStyle(
-                        color: Colors.grey[900],
-                        fontSize: 20,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Text(
-                    price.toDouble().toString(),
-                    style: TextStyle(
-                      color: Colors.grey[900],
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    change.toDouble() < 0
-                        ? change.toDouble().toString()
-                        : '+' + change.toDouble().toString(),
-                    style: TextStyle(
-                      color: change.toDouble() < 0 ? Colors.red : Colors.green,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    changePercentage.toDouble() < 0
-                        ? changePercentage.toDouble().toString() + '%'
-                        : '+' + changePercentage.toDouble().toString() + '%',
-                    style: TextStyle(
-                      color: changePercentage.toDouble() < 0
-                          ? Colors.red
-                          : Colors.green,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: ListView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: coinList.length,
+        itemBuilder: (context, index) {
+          return CoinCard(
+            name: coinList[index].name,
+            symbol: coinList[index].symbol,
+            image: coinList[index].image,
+            price: coinList[index].price.toDouble(),
+            change: coinList[index].change.toDouble(),
+            changePercentage: coinList[index].changePercentage.toDouble(),
+          );
+        },
       ),
     );
   }
